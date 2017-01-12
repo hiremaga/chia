dd2940e99e5841ab8833284afbce16e5cf7c4040
Abhi Hiremagalur <abhijit@hiremaga.com>
2017-01-12 16:50:38 +1100

    Update chia.sh

--- chia.sh
+++ chia.sh
@@ -13,3 +13,6 @@ xcode-select --install || true
 
 echo "CHIA: Updating OS X"
 softwareupdate -a -i true
+
+echo "CHIA: Installing Homebrew if it isn't already installed"
+which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

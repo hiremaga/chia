78de909648d5abc3745f680107a016614b78d9c3
Abhi Hiremagalur <abhijit@hiremaga.com>
2017-01-12 15:10:53 +1100

    Install Xcode Command Line Tools

--- chia.sh
+++ chia.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo say chia!
+echo
+echo      | |   (_)      
+echo   ___| |__  _  __ _ 
+echo  / __| '_ \| |/ _` |
+echo | (__| | | | | (_| |
+echo  \___|_| |_|_|\__,_|
+echo 
+
+echo "CHIA: Installing Xcode Command Line Tools if it isn't already installed"
+which gcc || sudo xcode-select --install

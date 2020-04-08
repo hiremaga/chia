dae276f41cb9e46dda3298405fc393e6020a0011
Abhi Hiremagalur <abhijit@hiremaga.com>
2020-04-08 10:29:11 -0700

    Rename, remove ASCII art & `brew bundle`
    - ASCII art breaks on Catalina
    - Skipping `brew bundle` allows script to be run without cloning / downloading this repo first

--- chia-bootstrap.sh
+++ chia-bootstrap.sh
@@ -1,13 +1,5 @@
 
-#!/bin/bash -xe
-
-echo
-echo      | |   (_)      
-echo   ___| |__  _  __ _ 
-echo  / __| '_ \| |/ _` |
-echo | (__| | | | | (_| |
-echo  \___|_| |_|_|\__,_|
-echo
+#!/bin/bash -ex
 
 echo "CHIA: Installing Xcode Command Line Tools if it isn't already installed"
 xcode-select --install || true
@@ -17,6 +9,3 @@ softwareupdate -a -i true
 
 echo "CHIA: Installing Homebrew if it isn't already installed"
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "CHIA: Installing/updating Homebrew taps, formulae and casks"
-brew bundle

39cb3a6ce12923a7fe39985419359748b87aac1c
Abhi Hiremagalur <abhijit@hiremaga.com>
2020-05-05 14:12:12 -0700

    Stop killing Spectacle to reload settings
    Workaround for https://github.com/hiremaga/chia/issues/2

--- macos-settings/spectacle-shortcuts.sh
+++ macos-settings/spectacle-shortcuts.sh
@@ -1,3 +1,4 @@
 cp -r macos-settings/spectacle-shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
 
-killall Spectacle &> /dev/null
+# TODO: https://github.com/hiremaga/chia/issues/2
+# killall Spectacle &> /dev/null

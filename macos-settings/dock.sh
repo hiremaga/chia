58d28bcbd3a47dbbe23ab6413040c5b61426409d
Abhi Hiremagalur <abhijit@hiremaga.com>
2020-05-06 11:10:07 -0700

    Clear standard Dock icons

--- macos-settings/dock.sh
+++ macos-settings/dock.sh
@@ -4,4 +4,7 @@ sudo defaults write /Library/Preferences/com.apple.dock autohide -bool YES
 # Automatically hide and show Dock.
 defaults write com.apple.dock autohide -bool true
 
+# Clear standard icons from Dock.
+defaults write com.apple.dock persistent-apps -array
+
 killall Dock &> /dev/null

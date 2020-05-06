bb5ee7e700d7dc96c7fa9f942dcae3fc0d40a7bd
Abhi Hiremagalur <abhijit@hiremaga.com>
2020-05-06 11:28:15 -0700

    Move sudo hack etc. to top-level script

--- chia.sh
+++ chia.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -ex
 
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Table stakes
 xcode-select --install || true
 softwareupdate -a -i true
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# First run?
 if [ ! -d ~/workspace/chia ]; then
   mkdir -p ~/workspace
   pushd ~/workspace
--- macos-settings/00-everything.sh
+++ macos-settings/00-everything.sh
@@ -1,13 +1,3 @@
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 source macos-settings/dock.sh
 source macos-settings/keyboard.sh
 source macos-settings/spectacle-shortcuts.sh

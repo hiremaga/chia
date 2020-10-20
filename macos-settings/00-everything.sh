dab8f07a7045c70ffe78342213e58e76c3f2b892
Abhi Hiremagalur <abhijit@hiremaga.com>
2020-10-20 16:01:24 -0700

    I started using shiftit accidentally and got used to it
    shiftit seems way simpler to install since it does not need reconfiguration

--- macos-settings/00-everything.sh
+++ macos-settings/00-everything.sh
@@ -1,6 +1,5 @@
 source macos-settings/dock.sh
 source macos-settings/keyboard.sh
-source macos-settings/spectacle-shortcuts.sh
 source macos-settings/switch-to-zsh.sh
 source macos-settings/install-flutter.sh
 source macos-settings/login-items.sh
--- macos-settings/spectacle-shortcuts.sh
+++
@@ -1,4 +0,0 @@
-cp -r macos-settings/spectacle-shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
-
-# TODO: https://github.com/hiremaga/chia/issues/2
-# killall Spectacle &> /dev/null

25c42101b5c7a31c973304da0c73c6e6f27b97ab
Abhi Hiremagalur <abhijit@hiremaga.com>
2021-07-12 17:25:16 -0700

    Add Gatsby

--- macos-tools/00-everything.sh
+++ macos-tools/00-everything.sh
@@ -5,3 +5,4 @@ source macos-tools/install-rubies.sh
 arch -x86_64 bash macos-tools/install-nodes.sh
 source macos-tools/install-openjdk.sh
 source macos-tools/install-jetbrains-ide-prefs.sh
+source macos-tools/install-global-npm-packages.sh

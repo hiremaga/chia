b40e281a4fee20a77f377445d02d9b07d1868c22
Abhi Hiremagalur <abhijit@hiremaga.com>
2021-11-30 12:23:38 -0800

    Prefer native ruby-install check

--- macos-tools/install-rubies.sh
+++ macos-tools/install-rubies.sh
@@ -1,2 +1,2 @@
-[ -d "$HOME/.rubies/ruby-2.7.2" ] || ruby-install ruby 2.7.2
-[ -d "$HOME/.rubies/ruby-3.0.0" ] || ruby-install ruby 3.0.0
+ruby-install ruby 2.7.2 --no-reinstall
+ruby-install ruby 3.0.0 --no-reinstall

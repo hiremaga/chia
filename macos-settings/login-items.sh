3bad182cddd094c5018d73697fce8c63188b6731
Abhi Hiremagalur <abhijit@hiremaga.com>
2022-02-26 16:18:27 -0800

    Use google-drive cask since google-back-and-sync is no longer a thing

--- macos-settings/login-items.sh
+++ macos-settings/login-items.sh
@@ -2,6 +2,6 @@ function login_item {
     osascript -e "tell application \"System Events\" to make login item at end with properties {path:\"$1\", hidden:false}"
 }
 
-login_item "/Applications/Backup and Sync.app"
+login_item "/Applications/Google Drive.app"
 login_item "/Applications/Flycut.app"
 login_item "/Applications/Spectacle.app"

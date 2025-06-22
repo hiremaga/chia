94ca825b7001ab36762edfc03b2efd4279ef2075
Abhi Hiremagalur <abhijit@hiremaga.com>
2025-06-21 17:39:04 -0700

    No need to use CloudFlare DNS servers, since I don't use xip.io; besides nip.io resolves fine with my new ISP

--- macos-settings/00-everything.sh
+++ macos-settings/00-everything.sh
@@ -1,4 +1,3 @@
-source macos-settings/dns.sh
 source macos-settings/dock.sh
 source macos-settings/keyboard.sh
 source macos-settings/switch-to-zsh.sh
--- macos-settings/dns.sh
+++
@@ -1,1 +0,0 @@
-networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1
